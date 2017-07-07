37dff71bdf12a93def072488721868dc2df9d14c
djholdski <alex@bahabar.com>
2017-07-07 12:18:27 -0400

    Allowed Hosts Test 3

--- peacockblog/settings.py
+++ peacockblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pisucrx!k9pq7131nhqrcor+dtgkfgh%*j0%^cfu-^h&tstpgj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [www.pythonanywhere.com]
+ALLOWED_HOSTS = ['charlyopolis.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
